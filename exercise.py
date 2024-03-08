0507db05e7c2e5d3cf10ae8b57254d28d7a18bd3
Muhammad Moraveji <ms.moraveji2007@gmail.com>
2024-03-09 01:43:37 +0330

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,7 +1,9 @@
 def divide_by_zero_safe():
+    #add try
     try:
         result = 10 / 0
         return result
+    #add except
     except ZeroDivisionError:
         print("Error: Division by zero occurred!")
 
